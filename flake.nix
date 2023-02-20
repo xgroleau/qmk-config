13f03830c3d1f26f9e6c8e278cbf19b66343f8be
xgroleau🐢 <xavgroleau@gmail.com>
2023-02-20 17:23:17 -0500

    working flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Build configuration from my RGBKB sol3";
+  description = "Build configuration from my qmk keyboards";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -9,11 +9,28 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
-
       in with pkgs; {
         devShells.default = mkShell {
-          nativeBuildInputs = with nixpkgs; [ qmk ];
-          QMK_HOME = ./qmk_firmware;
+          QMK_HOME = "./qmk_firmware";
+          nativeBuildInputs = [ qmk ];
         };
+
+        apps = let
+          flash = kb:
+            pkgs.writeShellApplication {
+              name = "flash";
+              runtimeInputs = with pkgs; [ qmk ];
+              text = ''
+                export QMK_HOME=./qmk_firmware
+                qmk flash -c ./${kb}.json
+              '';
+            };
+        in {
+          sol3 = {
+            type = "app";
+            program = "${flash "sol3"}/bin/flash";
+          };
+        };
+
       });
 }
