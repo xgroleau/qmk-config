5ac2fc8fe0d95090eaf089ceec81e059eb9acc24
xgroleau🐢 <xavgroleau@gmail.com>
2023-02-20 14:11:17 -0500

    init commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Build configuration from my RGBKB sol3";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+
+      in with pkgs; {
+        devShells.default = mkShell {
+          nativeBuildInputs = with nixpkgs; [ qmk ];
+
+        };
+      });
+}
